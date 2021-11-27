010a8e0a7624ad4f1056cb7cf298264bbccc0c73
LukasSalavejus <salavejus46@gmail.com>
2021-11-27 20:23:32 +0200

    Atliktas darbas

--- vaizdo_atpazinimas.m
+++ vaizdo_atpazinimas.m
@@ -1,36 +1,39 @@
 close all
 clear all
 clc
-%% raidþiø pavyzdþiø nuskaitymas ir poþymiø skaièiavimas
-%% read the image with hand-written characters
-pavadinimas = 'train_data.png';
+
+% 4 Laboratorinis darbas
+% Lukas Salavejus MNEfm-21
+
+% Neveikia su train_marker.jpg
+% pavadinimas = 'train_marker.jpg';
+
+% Padariau didesnius tarpus tarp raidziu - veikia
+pavadinimas = 'train_marker_edited.png';
+
 pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 8);
-%% Atpaþintuvo kûrimas
 %% Development of character recognizer
-% poþymiai ið celiø masyvo perkeliami á matricà
 % take the features from cell-type variable and save into a matrix-type variable
 P = cell2mat(pozymiai_tinklo_mokymui);
-% sukuriama teisingø atsakymø matrica: 11 raidþiø, 8 eilutës mokymui
 % create the matrices of correct answers for each line (number of matrices = number of symbol lines)
 T = [eye(11), eye(11), eye(11), eye(11), eye(11), eye(11), eye(11), eye(11)];
-% sukuriamas SBF tinklas duotiems P ir T sàryðiams
 % create an RBF network for classification with 13 neurons, and sigma = 1
+
 tinklas = newrb(P,T,0,1,13);
 
-%% Tinklo patikra | Test of the network (recognizer)
-% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+% Sumazinta neuronu iki 8, pirmo testo metu sumaiso H su A
+% tinklas = newrb(P,T,0,1,8);
+
+%% Test of the network (recognizer)
 % estimate output of the network for unknown symbols (row, that were not used during training)
 P2 = P(:,12:22);
 Y2 = sim(tinklas, P2);
-% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
 % find which neural network output gives maximum value
 [a2, b2] = max(Y2);
-%% Rezultato atvaizdavimas
+
 %% Visualize result
-% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
 % calculate the total number of symbols in the row
 raidziu_sk = size(P2,2);
-% rezultatà saugosime kintamajame 'atsakymas'
 % we will save the result in variable 'atsakymas'
 atsakymas = [];
 for k = 1:raidziu_sk
@@ -55,18 +58,18 @@ for k = 1:raidziu_sk
         case 9
             atsakymas = [atsakymas, 'I'];
         case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
             atsakymas = [atsakymas, 'J'];
+        case 11
+            atsakymas = [atsakymas, 'K'];
     end
 end
 % pateikime rezultatà komandiniame lange
 % show the result in command window
 disp(atsakymas)
-% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
-%% þodþio "KADA" poþymiø iðskyrimas 
+figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
+
 %% Extract features of the test image
-pavadinimas = 'test_kada.png';
+pavadinimas = 'test_marker1.png';
 pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
 
 %% Raidþiø atpaþinimas
@@ -107,9 +110,9 @@ for k = 1:raidziu_sk
         case 9
             atsakymas = [atsakymas, 'I'];
         case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
             atsakymas = [atsakymas, 'J'];
+        case 11
+            atsakymas = [atsakymas, 'K'];
     end
 end
 % pateikime rezultatà komandiniame lange
@@ -117,7 +120,7 @@ end
 figure(8), text(0.1,0.5,atsakymas,'FontSize',38), axis off
 %% þodþio "FIKCIJA" poþymiø iðskyrimas 
 %% extract features for next/another test image
-pavadinimas = 'test_fikcija.png';
+pavadinimas = 'test_marker2.png';
 pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
 
 %% Raidþiø atpaþinimas
@@ -153,9 +156,9 @@ for k = 1:raidziu_sk
         case 9
             atsakymas = [atsakymas, 'I'];
         case 10
-            atsakymas = [atsakymas, 'K'];
-        case 11
             atsakymas = [atsakymas, 'J'];
+        case 11
+            atsakymas = [atsakymas, 'K'];
     end
 end
 % pateikime rezultatà komandiniame lange
